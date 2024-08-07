aba66816761575beffc8aa0b144f8fbcbffbaded
Duc.NT <311020ntd@gmail>
2024-08-07 19:55:44 +0700

    bmi

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿double bmi;
+Console.WriteLine("Please enter a height");
+float height = float.Parse(Console.ReadLine());
+Console.WriteLine("Please enter your weight");
+float weight = float.Parse(Console.ReadLine());;
+ bmi = weight / (height*height);
+Console.WriteLine(bmi);
+if (bmi < 18.5)
+{
+    Console.WriteLine("Underweight");
+}
+else if (bmi < 25.0)
+{
+    Console.WriteLine(" Normal");
+}
+else if (bmi < 30.0)
+{
+    Console.WriteLine(" Overweight");
+
+}
+else
+{
+    Console.WriteLine(" Obese");
+}
